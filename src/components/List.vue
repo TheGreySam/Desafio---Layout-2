6912e44f47075bb76a53d4111489a8e94fef514e
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-10-07 20:25:33 -0300

    commit 2

---
+++ src/components/List.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Hola que hace</div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <b-navbar class="has-background-success">
+        <template #brand >
+            <b-navbar-item tag="router-link" :to="{ path: '/' }" class="is-size-2 has-text-white">
+                Rick and Morty
+            </b-navbar-item>
+            
+        </template>
+        
+    </b-navbar>
+    
+</template>
